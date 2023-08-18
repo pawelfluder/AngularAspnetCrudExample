e70d71a8d824391fad4c5146d90c7a6facbbecdd
Build Service <buildservice@mvpprojects.com>
2023-08-18 20:04:15 +0200

    Update

---
+++ AspNet.API/AspNetWebApi/AspNetWebApi/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetWebApi.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
